9cf71e559623ebb8fcf39b776744e26844166552
andered <86933794+andered@users.noreply.github.com>
2022-09-02 08:12:32 -0400

    new working version, revision of the interface

--- pdf_image_extraction.py
+++ pdf_image_extraction.py
@@ -5,27 +5,37 @@ import fitz
 from PIL import Image
 from os import listdir
 
+
+st.set_page_config(page_title='PDF Image Extraction')
 st.title('PDF Image Extraction')
-st.markdown('Images will be extracted from an uploaded PDF')
-st.markdown('Right click on images and save to file')
 
-uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 
+with st.sidebar:
+    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+    #st.markdown('Images will be extracted from an uploaded PDF')
+    st.markdown('**Right click on images and save to file**')
+
+    
 if uploaded_file is not None:
     file_details = {"filename":uploaded_file.name, "filetype":uploaded_file.type,"filesize":uploaded_file.size}
-    st.write(file_details)
+    st.write("Filename: ",uploaded_file.name)
+    st.write("File type: ", uploaded_file.type)
+    st.write("File size: ", uploaded_file.size)
+    
+    #st.json(file_details)
     pdf_file = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    image_count = 0
     
     for page_index in range(len(pdf_file)):
         # get the page itself
         page = pdf_file[page_index]
         image_list = page.get_images()
         # printing number of images found in this page
-        if image_list:
-            st.markdown(f"**[+] Found {len(image_list)} images in page {page_index}**")
-        else:
-            st.markdown("[!] No images found on page", page_index)
-        
+        #if image_list:
+          #  st.markdown(f"**[+] Found {len(image_list)} images in page {page_index}**")
+      #  else:
+           # st.markdown(f"**[!] No images found on page {page_index}**")
+    
         for image_index, img in enumerate(page.get_images(), start=1):
             # get the XREF of the image
             xref = img[0]
@@ -36,8 +46,12 @@ if uploaded_file is not None:
             image_ext = base_image["ext"]
             # load it to PIL
             image = Image.open(io.BytesIO(image_bytes))
-            st.write('page: ', page_index+1, 'image number: ', image_index,'file type: ', image_ext)
-            st.image(image, width=500, caption= (f"page: {page_index+1} / image number:  {image_index} / file type: {image_ext}"))
+            image_count=image_count+1
+            st.markdown(f"image number: {image_count} / file type:  {image_ext} / page: {page_index+1} ")
+            #st.image(image, width=500, caption= (f"page: {page_index+1} / image number:  {image_count} / file type: {image_ext}"))
+            with st.container():
+                st.image(image, width=500)
+                st.markdown("""---""")
     
 
     
